60df9626bc47ff699cb53622f0aa6df35a4a0e05
milenalima <milenalima.lopes@hotmail.com>
2018-09-18 08:43:52 -0300

    Implementação da classa cordão.

--- app/src/main/java/com/example/milenal/listoapp/user/Cordon.java
+++ app/src/main/java/com/example/milenal/listoapp/user/Cordon.java
@@ -1,4 +1,43 @@
 package com.example.milenal.listoapp.user;
 
+import java.util.Date;
+
 public class Cordon {
+
+    private String nome;
+    private String item;
+    private Date data;
+
+    public Cordon() {
+    }
+
+    public Cordon(String nome, String item, Date data) {
+        this.nome = nome;
+        this.item = item;
+        this.data = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
