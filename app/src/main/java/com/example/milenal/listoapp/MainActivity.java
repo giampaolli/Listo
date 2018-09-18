e9e05b4762c949982c0bf181790c0ed505c17622
milenalima <milenalima.lopes@hotmail.com>
2018-09-18 12:47:09 -0300

    alterações mais tela esqueci senha

--- app/src/main/java/com/example/milenal/listoapp/MainActivity.java
+++ app/src/main/java/com/example/milenal/listoapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.milenal.listoapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.milenal.listoapp.conection.Conection;
+import com.example.milenal.listoapp.user.Progression;
+import com.example.milenal.listoapp.user.ResetSenha;
 import com.example.milenal.listoapp.user.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,22 +25,69 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonEntrar;
     private TextView textEmail, textSenha, textEsqueciSenha, textNovoUsuario;
 
+    private FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startComponents();
-        clickEvent();
+//        clickEvent();
     }
 
-    private void clickEvent() {
-        textNovoUsuario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),User.class);
-                startActivity(intent);
-            }
-        });
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, User.class));
+    }
+
+    public void btEsqueciSenha(View view){
+        startActivity(new Intent(this, EsqueciSenha.class));
+    }
+
+//    private void clickEvent() {
+//        textNovoUsuario.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(),User.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        buttonEntrar.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//               String email = editEmail.getText().toString().trim();
+//               String senha = editSenha.getText().toString().trim();
+//               login(email, senha);
+//            }
+//        });
+//
+//        textEsqueciSenha.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ResetSenha.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+
+    private void login(String email, String senha) {
+        auth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this, Progression.class);
+                            startActivity(intent);
+                        }else{
+                            alert("Email ou senha inválidos");
+                        }
+                    }
+                });
+    }
+
+    private void alert(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
     private void startComponents(){
@@ -42,5 +98,11 @@ public class MainActivity extends AppCompatActivity {
         textEsqueciSenha = findViewById(R.id.txtEsqueciSenha);
         textNovoUsuario = findViewById(R.id.txtNovoUsuario);
         buttonEntrar = findViewById(R.id.btnEntrar);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = Conection.getFirebaseAuth();
     }
 }
---
+++ app/src/main/java/com/example/milenal/listoapp/user/ResetSenha.java
@@ -0,0 +1,22 @@
+package com.example.milenal.listoapp.user;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.milenal.listoapp.R;
+
+public class ResetSenha extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private Button btnReset;
+
+    /*@Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.esqueci_senha);
+
+    }*/
+}
