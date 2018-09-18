2a55d8b120469e9466b3d8481c055ef13bc3214a
milenalima <milenalima.lopes@hotmail.com>
2018-09-18 08:45:57 -0300

    Implementação da classe cargo.

--- app/src/main/java/com/example/milenal/listoapp/user/Office.java
+++ app/src/main/java/com/example/milenal/listoapp/user/Office.java
@@ -1,4 +1,31 @@
 package com.example.milenal.listoapp.user;
 
 public class Office {
+
+    private String nome;
+    private String descricao;
+
+    public Office() {
+    }
+
+    public Office(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
