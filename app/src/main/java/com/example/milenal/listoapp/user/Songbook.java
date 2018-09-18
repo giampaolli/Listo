8e38292c23416d29df4dec280df650bd6ae25208
milenalima <milenalima.lopes@hotmail.com>
2018-09-18 08:48:12 -0300

    Implementação da classe cancioneiro.

--- app/src/main/java/com/example/milenal/listoapp/user/Songbook.java
+++ app/src/main/java/com/example/milenal/listoapp/user/Songbook.java
@@ -1,4 +1,21 @@
 package com.example.milenal.listoapp.user;
 
 public class Songbook {
+
+    private String titulo;
+
+    public Songbook() {
+    }
+
+    public Songbook(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
