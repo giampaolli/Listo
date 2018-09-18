08606143757b57a1daa40118799df62b8133b0db
milenalima <milenalima.lopes@hotmail.com>
2018-09-18 08:45:13 -0300

    Implementação da classe biblioteca.

--- app/src/main/java/com/example/milenal/listoapp/user/Library.java
+++ app/src/main/java/com/example/milenal/listoapp/user/Library.java
@@ -1,4 +1,21 @@
 package com.example.milenal.listoapp.user;
 
 public class Library {
+
+    private String titulo;
+
+    public Library() {
+    }
+
+    public Library(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
