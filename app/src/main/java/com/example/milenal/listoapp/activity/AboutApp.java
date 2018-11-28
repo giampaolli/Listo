496d5b53965795d1540ea651e462e5cb4437304f
milenalima <milenalima.lopes@hotmail.com>
2018-11-28 01:35:28 -0200

    Implementação da tela Sobre.

--- app/src/main/java/com/example/milenal/listoapp/activity/AboutApp.java
+++ app/src/main/java/com/example/milenal/listoapp/activity/AboutApp.java
@@ -2,6 +2,7 @@ package com.example.milenal.listoapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.milenal.listoapp.R;
@@ -14,11 +15,16 @@ public class AboutApp extends AppCompatActivity {
         setContentView(R.layout.activity_about_app);
 
         TextView aboutApp = findViewById(R.id.txtAbouApp);
+        ImageView listo = findViewById(R.id.listo);
 
         aboutApp.setText("Trabalho de Conclusão de Curso realizado por Milena Lima Lopes." +
                 "Orientado pelo Professor Tiago Souza. " +
                 "A intenção deste aplicativo foi contribuir com a principal proposta da UEB " +
                 "(União dos Escoteiros do Brasil), fazer com que o jovem assuma a própria " +
                 "resposábilidade e desenvolvendo assim suas próprias habilidades.");
+
+        listo.setImageResource(R.drawable.listo);
+
+
     }
 }
